af618726413e79bff36423f96a72e7b62bda2aa7
Margatroid <i@margatroid.xyz>
2020-06-12 13:47:52 +0800

    feat: initial working version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,183 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect, useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Paper from '@material-ui/core/Paper'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
+import CanvasDraw from "react-canvas-draw"
+import * as tf from '@tensorflow/tfjs'
+import LinearProgress from '@material-ui/core/LinearProgress'
+import Snackbar from '@material-ui/core/Snackbar'
+import Alert from '@material-ui/lab/Alert'
+import Box from '@material-ui/core/Box'
+import useMediaQuery from '@material-ui/core/useMediaQuery'
+import useTheme from '@material-ui/core/styles/useTheme'
 
-function App() {
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    position: 'relative',
+  },
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing(2),
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
+      width: '80vw',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  stepper: {
+    padding: theme.spacing(3, 0, 5),
+  },
+  buttons: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+  button: {
+    marginTop: theme.spacing(3),
+    marginLeft: theme.spacing(1),
+  },
+  paper: {
+    marginTop: theme.spacing(3),
+    padding: theme.spacing(2),
+    width: 'auto',
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      padding: theme.spacing(3),
+    },
+  },
+  canvasPaper: {
+    marginTop: theme.spacing(3),
+    marginBottom: theme.spacing(3),
+    padding: theme.spacing(2),
+    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
+      marginTop: theme.spacing(6),
+      marginBottom: theme.spacing(6),
+      width: '448px',
+      padding: theme.spacing(3),
+    },
+  },
+  canvasElement: {
+    margin: '0 auto'
+  },
+  btnGroup: {
+    margin: "2em 1em"
+  }
+}));
+
+function ProbIndicator({ v, i }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box display="block" alignItems="center">
+      <Box width="100%" mr={1}>
+        <LinearProgress variant="determinate" value={v * 100} />
+      </Box>
+      <Box minWidth={35}>
+        <Typography variant="body2" color="textSecondary">{`${i}: ${Math.round(
+          v * 100,
+        )}%`}</Typography>
+      </Box>
+    </Box>
+  )
 }
 
-export default App;
+export default function App() {
+  const theme = useTheme()
+  const isSM = useMediaQuery(theme.breakpoints.down('sm'))
+  const canvansSize = isSM ? 300 : 400
+  const classes = useStyles()
+  const canvasRef = useRef(null)
+  const [model, setModel] = useState(null)
+  const [open, setOpen] = useState(false)
+  const [predRes, setPredRes] = useState([])
+  useEffect(() => {
+    async function loadModel() {
+      setModel(await tf.loadLayersModel('/model.json'))
+      setOpen(true)
+    }
+    loadModel()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+  async function recognize() {
+    const image = tf.browser.fromPixels(canvasRef.current.canvasContainer.children[1])
+    const img = tf.image.resizeBilinear(image, [28, 28])
+      .toFloat()
+      .mean(2)
+      .divNoNan(255)
+      .reshape([1, 28, 28, 1])
+    await tf.browser.toPixels(img.reshape([28, 28]), document.getElementById('qwq'))
+    const res = model.predict(img)
+    setPredRes((await res.array())[0])
+    image.dispose()
+    img.dispose()
+    res.dispose()
+  }
+  return (
+    <>
+      <CssBaseline />
+      <Snackbar open={open} autoHideDuration={3000} onClose={() => { setOpen(false) }}>
+        <Alert severity="success" elevation={6} variant="filled">
+          模型加载成功
+        </Alert>
+      </Snackbar>
+      {!model && <LinearProgress style={{ zIndex: 2000 }} />}
+      <AppBar position="absolute" color="default" className={classes.appBar}>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            在线识别手写数字
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main className={classes.layout}>
+        <Grid container spacing={2}>
+          <Grid item>
+            <Paper className={classes.canvasPaper}>
+              <CanvasDraw
+                className={classes.canvasElement}
+                ref={canvasRef}
+                canvasHeight={canvansSize}
+                canvasWidth={canvansSize}
+                brushColor="#aaa"
+                onChange={recognize}
+              />
+              <canvas id="qwq" width={28} height={28} />
+            </Paper>
+          </Grid>
+          <Grid item xs>
+            <Paper className={classes.paper}>
+              <ButtonGroup color="primary" className={classes.btnGroup}>
+                <Button onClick={
+                  () => {
+                    canvasRef.current.undo()
+                  }
+                }>撤销</Button>
+                <Button onClick={
+                  () => {
+                    canvasRef.current.clear()
+                  }
+                }>重画</Button>
+                <Button onClick={recognize}> 识别</Button>
+              </ButtonGroup>
+              {
+                <Grid container spacing={2}>
+                  {predRes.map(
+                    (v, i) => (
+                      <Grid item xs={6} key={i}>
+                        <ProbIndicator v={v} i={i} />
+                      </Grid>
+                    )
+                  )}
+                </Grid>
+              }
+            </Paper>
+          </Grid>
+        </Grid>
+      </main>
+    </>
+  );
+}
